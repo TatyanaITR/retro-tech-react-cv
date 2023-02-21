677f6b70a162dca49baebaa117c317f0c100e292
Tatyana <tr.itrnow@gmail.com>
2023-02-22 01:57:41 +0300

    Initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    "postcss-custom-media": {},
+    "postcss-import": {},
+    "postcss-mixins": {},
+    "postcss-nesting": {},
+    "postcss-preset-env": {
+      stage: 2,
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <p>This is the base react-ts boilerplate</p>
+    </>
+  );
+}
+
+export default App;
